cdce5b1ddda44309bf6042066fc5efb1c8d51560
GreenNinjaBoy <>
2023-09-13 10:44:22 +0000

    asset(script.js): create const variable for trivia game and began on inital event listener for game to begin

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,31 @@
-function ruleDisplay() {
+const triviaHeading = document.getElementById("trivia-heading");
+const triviaMenu = document.getElementById("trivia-menu");
+const gameRules = document.getElementById("game-rules");
+const difficultySetting = document.getElementById("difficulty-setting");
+const triviaArea = document.getElementById("trivia-area");
+const mainAnswerArea = document.getElementById("main-answer-area");
+
+
+/* wait for the DOM to succesfully load before executing the first function
+which will deliver the menu buttons to the user and add event liseners to these buttons*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButtons = this.querySelectorAll(".menu-btn");
+    menuButtons.forEach(menuButton) => {
+        menuButton.addEventListener("click", function () {
+            if (this.getAttribute("id") === "rules-btn") {
+                displayGameRules();
+            } else if (this.getAttribute("id") === "trivia-btn") {
+                triviaDifficultyPromt();
+            }
+        });
+    }
+});
+
+
+
+
+function displayGameRules() {
 
 }
 
@@ -6,7 +33,7 @@ function closeRules() {
 
 }
 
-function difficultyPromt () {
+function triviaDifficultyPromt() {
 
 }
 
@@ -39,5 +66,5 @@ function displayTriviaContent() {
 }
 
 function displayQuestionNumber() {
-    
+
 }
