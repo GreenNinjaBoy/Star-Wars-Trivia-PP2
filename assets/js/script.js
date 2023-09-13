d1ef87a916f1edcee61acca8550b7f21566de217
GreenNinjaBoy <>
2023-09-13 12:26:28 +0000

    asset(script.js): created function to allow user to access rules and tested

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const triviaHeading = document.getElementById("trivia-heading");
 const triviaMenu = document.getElementById("trivia-menu");
-const gameRules = document.getElementById("game-rules");
+const gameRules = document.getElementById("rules");
 const difficultySetting = document.getElementById("difficulty-setting");
 const triviaArea = document.getElementById("trivia-area");
 const mainAnswerArea = document.getElementById("main-answer-area");
@@ -11,7 +11,7 @@ which will deliver the menu buttons to the user and add event liseners to these 
 
 document.addEventListener("DOMContentLoaded", function () {
     const menuButtons = this.querySelectorAll(".menu-btn");
-    menuButtons.forEach(menuButton) => {
+    menuButtons.forEach((menuButton) => {
         menuButton.addEventListener("click", function () {
             if (this.getAttribute("id") === "rules-btn") {
                 displayGameRules();
@@ -19,17 +19,29 @@ document.addEventListener("DOMContentLoaded", function () {
                 triviaDifficultyPromt();
             }
         });
-    }
+    });
 });
 
 
-
-
+/**
+ * Whilst displaying the rules, the main menu will be hiddden
+ * ann wil add an event listener for the close button.
+ */
 function displayGameRules() {
+    gameRules.classList.remove("hide");
+    triviaHeading.classList.add("hide");
+    triviaMenu.classList.add("hide");
+    const closeRulesButton = document.getElementById("close-rules");
+    closeRulesButton.addEventListener("click", closeRules);
 
 }
-
+/**
+ * This function will hide the rules and displays the main menu.
+ */
 function closeRules() {
+    gameRules.classList.add("hide");
+    triviaHeading.classList.remove("hide");
+    triviaMenu.classList.remove("hide");
 
 }
 
