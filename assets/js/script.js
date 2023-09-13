40b8d990d3b39e5ec190286ca1b1f2095d92d7ef
GreenNinjaBoy <>
2023-09-13 09:48:31 +0000

    asset(script.js): created empty functions with relevant names to what is needed for trivia to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,43 @@
-console.log("Running");
+function ruleDisplay() {
+
+}
+
+function closeRules() {
+
+}
+
+function difficultyPromt () {
+
+}
+
+function closeDifficultyPromt() {
+
+}
+
+function closeTrivia() {
+
+}
+
+function startPadawanTrivia() {
+
+}
+
+function startJediKnightTrivia() {
+
+}
+
+function startJediMasterTrivia() {
+
+}
+
+function nextQuestion() {
+
+}
+
+function displayTriviaContent() {
+
+}
+
+function displayQuestionNumber() {
+    
+}
