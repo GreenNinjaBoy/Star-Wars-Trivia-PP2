725b3eaa1aed7976a5d8f09df98585039df2c53c
GreenNinjaBoy <>
2023-09-14 08:54:25 +0000

    asset(questions.js): created new js file for question bank and created object for easy mode.

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+/* I used the following youtube video to help
+   with ideas when implementing the questions for the trivia:
+   https://www.youtube.com/watch?v=riDzcEQbX6k*/
+
+// Creating an object containing the Padawan (easy) questions.
+
+const padawanQuestions = [
+    {
+        question: "What color is yodas lightsaber?",
+        answers: [
+            { text: "Green", correct: true },
+            { text: "Blue", correct: false },
+            { text: "Yellow", correct: false },
+            { text: "Red", correct: false },
+        ]
+    }
+];
