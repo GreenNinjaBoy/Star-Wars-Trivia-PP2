e32de83781be82236c5a9da869eee3fcdc3508aa
jamie connell <Jamie.Connell_10@hotmail.com>
2023-09-20 19:47:25 +0000

    asset(sounds.js): removed hover sound on begin quiz button.

--- assets/js/sound.js
+++
@@ -1,8 +0,0 @@
-// Get the button and audio elements
-const button = document.querySelector('.animated-btn');
-const hoverSound = document.getElementById('hoverSound');
-
-// Add a event listener to play the sound on hover
-button.addEventListener('hover', function () {
-    hoverSound.play();
-});
